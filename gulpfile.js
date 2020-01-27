fa47430af7ccdc3828ae006d69aadee8b119ae91
Anthony Mattera <contact@retroweeb.com>
2020-01-26 20:46:53 -0500

    Easier Build Pipeline, OJD Installer Packages for OSes that support them.

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,6 @@ function taskError(e) {
 	console.error(e);
 }
 
-function sectionOutput(message) {
-	const time = new Date().toUTCString();
-	console.log('----------------------------------------------------');
-	console.log(`[${time}]	${message}`);
-	console.log('----------------------------------------------------');
-}
-
 function taskCSS(){
 	return combiner.obj([
 		gulp.src('./app/stylesheets/less/main.less'),
@@ -32,132 +25,6 @@ function taskWatch() {
 		console.log(`Updated: ${name}`);
 		taskCSS()
 	});
-}
-
-function taskBuildLinux() {
-
-	if (os.platform() !== 'linux') {
-		taskError("Try this command again while you're in linux chief.");
-		return;
-	}
-
-	sectionOutput('Start Building (Linux)');
-
-	let version = 0;
-	let unstable = '';
-
-	sectionOutput('Transforming LESS Files');
-	taskCSS();
-
-	sectionOutput('Rebuilding Electron');
-	taskRebuildElectron();
-
-	const versionFile = fs.openSync('app/version', 'r');
-	version = fs.readFileSync(versionFile, 'UTF-8');
-	fs.closeSync(versionFile);
-
-	sectionOutput('Cleaning Artifacts');
-	execSync('rm -Rfv ./dist/', {stdio: 'inherit'});
-	sectionOutput('Building Package');
-	execSync('yarn build', {stdio: 'inherit'});
-	if (process.arch === 'x64') {
-		execSync('mv ./dist/linux-unpacked ./dist/open-joystick-display', {stdio: 'inherit'});
-	} else {
-		execSync('mv ./dist/linux-ia32-unpacked ./dist/open-joystick-display', {stdio: 'inherit'});
-	}
-
-	sectionOutput('Copying Icon');
-	execSync('cp ./app/icons/icon.png ./dist/open-joystick-display/icon.png', {stdio: 'inherit'});
-	sectionOutput('Compressing Package');
-
-	if (process.arch === 'x64') {
-		execSync(`tar czvf ./dist/open-joystick-display-${version}${unstable}-x64-linux.tar.gz -C ./dist open-joystick-display`, {stdio: 'inherit'});
-	} else {
-		execSync(`tar czvf ./dist/open-joystick-display-${version}${unstable}-x86-linux.tar.gz -C ./dist open-joystick-display`, {stdio: 'inherit'});
-	}
-
-	sectionOutput('Build Finished');
-
-}
-
-function taskBuildWindows() {
-
-	if (os.platform() !== 'win32') {
-		taskError("Try this command again while you're in windows chief.");
-		return;
-	}
-
-	sectionOutput('Start Building (Win32)');
-
-	let version = 0;
-	let unstable = '';
-
-	sectionOutput('Transforming LESS Files');
-	taskCSS();
-
-	sectionOutput('Rebuilding Electron');
-	taskRebuildElectron();
-
-	const versionFile = fs.openSync('app/version', 'r');
-	version = fs.readFileSync(versionFile, 'UTF-8');
-	fs.closeSync(versionFile);
-
-	sectionOutput('Cleaning Artifacts');
-	try {
-		execSync('rmdir /S /Q .\\dist', {stdio: 'inherit'});
-	} catch (error){
-		console.log('No dist directory to delete. Moving on...');
-	}
-	sectionOutput('Building Package');
-	execSync('yarn build', {stdio: 'inherit'});
-	if (process.arch === 'x64') {
-		execSync('rename .\\dist\\win-unpacked open-joystick-display', {stdio: 'inherit'});
-	} else {
-		execSync('rename .\\dist\\win-ia32-unpacked open-joystick-display', {stdio: 'inherit'});
-	}
-	sectionOutput('Compressing Package');
-
-	if (process.arch === 'x64') {
-		execSync(`tools\\windows\\7zip\\7za.exe a -r .\\dist\\open-joystick-display-${version}${unstable}-x64-windows.zip .\\dist\\open-joystick-display`, {stdio: 'inherit'});
-	} else {
-		execSync(`tools\\windows\\7zip\\7za.exe a -r .\\dist\\open-joystick-display-${version}${unstable}-x86-windows.zip .\\dist\\open-joystick-display`, {stdio: 'inherit'});
-	}
-
-	sectionOutput('Build Finished');
-
-}
-
-function taskBuildDarwin() {
-
-	if (os.platform() !== 'darwin') {
-		taskError("Try this command again while you're in macOS chief.");
-		return;
-	}
-
-	sectionOutput('Start Building (Darwin/macOS)');
-
-	let version = 0;
-	let unstable = '';
-
-	sectionOutput('Transforming LESS Files');
-	taskCSS();
-
-	sectionOutput('Rebuilding Electron');
-	taskRebuildElectron();
-
-	const versionFile = fs.openSync('app/version', 'r');
-	version = fs.readFileSync(versionFile, 'UTF-8');
-	fs.closeSync(versionFile);
-
-	sectionOutput('Cleaning Artifacts');
-	execSync('rm -Rfv ./dist/', {stdio: 'inherit'});
-	sectionOutput('Building Package');
-	execSync('yarn build', {stdio: 'inherit'});
-	sectionOutput('Renaming Package');
-	execSync(`mv ./dist/*.dmg ./dist/open-joystick-display-${version}${unstable}-x64-mac.dmg`, {stdio: 'inherit'});
-
-	sectionOutput('Build Finished');
-
 }
 
 function taskRebuildElectron() {
@@ -178,16 +45,7 @@ gulp.task('default', function(cb) {
 });
 
 gulp.task('build', function(cb) {
-	const platform = os.platform();
-	if (platform === 'win32') {
-		taskBuildWindows();
-	} else if (platform === 'linux') {
-		taskBuildLinux();
-	} else if (platform === 'darwin') {
-		taskBuildDarwin();
-	} else {
-		console.error('Unsupported Platform');
-	}
+	taskCSS();
 	cb();
 });
 
@@ -196,17 +54,3 @@ gulp.task('rebuild-electron', function(cb) {
 	cb();
 });
 
-gulp.task('build-linux', function(cb) {
-	taskBuildLinux();
-	cb();
-});
-
-gulp.task('build-windows', function(cb) {
-	taskBuildWindows();
-	cb();
-});
-
-gulp.task('build-darwin', function(cb) {
-	taskBuildDarwin();
-	cb();
-});
